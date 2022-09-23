f8941847bdf586f1ce0af3e0303ab6b1f24e21a3
RMVivek <vivekrm@tridentnets.in>
2022-09-23 21:54:09 +0530

    new

--- src/upload2.js
+++ src/upload2.js
@@ -1,54 +1,54 @@
-// 
-import './App.css';
-import { useState } from "react";
-import { storage } from './firebase';
-import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import "./App.css";
+import { useState, useEffect } from "react";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  listAll,
+  list,
+} from "firebase/storage";
+import { storage } from "./firebase";
+import { v4 } from "uuid";
 
 function App() {
-  const [imgUrl, setImgUrl] = useState(null);
-  const [progresspercent, setProgresspercent] = useState(0);
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageUrls, setImageUrls] = useState([]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const file = e.target[0]?.files[0]
-    if (!file) return;
-    const storageRef = ref(storage, `files/${file.name}`);
-    const uploadTask = uploadBytesResumable(storageRef, file);
+  const imagesListRef = ref(storage, "images/");
+  const uploadFile = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls((prev) => [...prev, url]);
+      });
+    });
+  };
 
-    uploadTask.on("state_changed",
-      (snapshot) => {
-        const progress =
-          Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
-        setProgresspercent(progress);
-      },
-      (error) => {
-        alert(error);
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          setImgUrl(downloadURL)
+  useEffect(() => {
+    listAll(imagesListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageUrls((prev) => [...prev, url]);
         });
-      }
-    );
-  }
+      });
+    });
+  }, []);
 
   return (
     <div className="App">
-      <form onSubmit={handleSubmit} className='form'>
-        <input type='file' />
-        <button type='submit'>Upload</button>
-      </form>
-      {
-        !imgUrl &&
-        <div className='outerbar'>
-          <div className='innerbar' style={{ width: `${progresspercent}%` }}>{progresspercent}%</div>
-        </div>
-      }
-      {
-        imgUrl &&
-        <img src={imgUrl} alt='uploaded file' height={200} />
-      }
+      <input
+        type="file"
+        onChange={(event) => {
+          setImageUpload(event.target.files[0]);
+        }}
+      />
+      <button onClick={uploadFile}> Upload Image</button>
+      {imageUrls.map((url) => {
+        return <img src={url} />;
+      })}
     </div>
   );
 }
+
 export default App;
